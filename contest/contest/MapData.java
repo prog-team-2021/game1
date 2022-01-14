b508223944e21cd45e66a89477fbac702e20c7a0
Maxel31 <86921026+Maxel31@users.noreply.github.com>
2022-01-14 11:33:24 +0900

    //

--- contest/contest/MapData.java
+++ contest/contest/MapData.java
@@ -1,21 +1,33 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
 import java.util.Random;
 
+
 public class MapData {
-    public static final int TYPE_SPACE = 0;
-    public static final int TYPE_WALL = 1;
-    public static final int TYPE_OTHERS = 2;
-    
+   public static final int TYPE_SPACE = 0;
+   public static final int TYPE_WALL = 1;
+   public static final int TYPE_GATE = 2;
+   public static final int TYPE_SWITCH = 3;
+   public static final int TYPE_TURN = 4;
+   public static final int TYPE_KEY = 5;
+   private static final int NUM = 6;
+
+   //Count
+   public MapGameController controller;
+   public int getKeyNum(){
+       return controller.KeyNum;
+   }
+   public int getCoinNum(){
+       return controller.CoinNum;
+   }
+   //
     private static final String mapImageFiles[] = {
         "png/SPACE.png",
         "png/WALL.png",
         "png/gate.png",
         "png/switch.png",
-        "png/turn.png"
+        "png/turn.png",
+        "png/key.png"
     };
 
     private Image[] mapImages;
@@ -24,115 +36,152 @@ public class MapData {
     private int width;
     private int height;
 
-    //status
-    private int ScoreNum=0;
-    private int StageNum=0;
+    //random
+    Random rnd = new Random();
+    int x_rnd;
+    int y_rnd;
+    boolean isExistKey;
+    boolean isExistSwitch;
     //
+
     MapData(int x, int y){
-        mapImages = new Image[5];
-        mapImageViews = new ImageView[y][x];
-        for (int i=0; i<5; i++) {
-            mapImages[i] = new Image(mapImageFiles[i]);
-        }
+       mapImages = new Image[NUM];
+       mapImageViews = new ImageView[y][x];
+       for (int i=0; i<NUM; i++) {
+           mapImages[i] = new Image(mapImageFiles[i]);
+       }
 
-        width = x;
-        height = y;
-        maps = new int[y][x];
+       width = x;
+       height = y;
+       maps = new int[y][x];
 
-        fillMap(MapData.TYPE_WALL);
-        digMap(1, 3);
-        setImageViews();
-        
-    }
+       fillMap(MapData.TYPE_WALL);
+       digMap(1, 3);
+       putturn();
+       putswitch();
+       putgate();
+       putkey();
+       setImageViews();
+   }
 
-    public int getHeight(){
-        return height;
-    }
+   public int getHeight(){
+       return height;
+   }
 
-    public int getWidth(){
-        return width;
-    }
+   public int getWidth(){
+       return width;
+   }
 
-    public int getMap(int x, int y) {
-        if (x < 0 || width <= x || y < 0 || height <= y) {
-            return -1;
-        }
-        return maps[y][x];
-    }
+   public int getMap(int x, int y) {
+       if (x < 0 || width <= x || y < 0 || height <= y) {
+           return -1;
+       }
+       return maps[y][x];
+   }
 
-    //getstatus
-      public int getScoreNum(){
-        return ScoreNum;
-    }
+   public ImageView getImageView(int x, int y) {
+       return mapImageViews[y][x];
+   }
 
-    public int getStageNum(){
-        return StageNum;
-    }
-    //
-    public ImageView getImageView(int x, int y) {
-        return mapImageViews[y][x];
-    }
+   public void setMap(int x, int y, int type){
+       if (x < 1 || width <= x-1 || y < 1 || height <= y-1) {
+           return;
+       }
+       maps[y][x] = type;
+   }
 
-    public void setMap(int x, int y, int type){
-        if (x < 1 || width <= x-1 || y < 1 || height <= y-1) {
-            return;
-        }
-        maps[y][x] = type;
-    }
+ // set images based on two-dimentional arrays (maps[y][x])
+   public void setImageViews() {
+       for (int y=0; y<height; y++) {
+           for (int x=0; x<width; x++) {
+               mapImageViews[y][x] = new ImageView(mapImages[maps[y][x]]);
+           }
+       }
+   }
 
-	// set images based on two-dimentional arrays (maps[y][x])
-    public void setImageViews() {
-        for (int y=0; y<height; y++) {
-            for (int x=0; x<width; x++) {
-                mapImageViews[y][x] = new ImageView(mapImages[maps[y][x]]);
-            }
-        }
-    }
+ // fill two-dimentional arrays with a given number (maps[y][x])
+   public void fillMap(int type){
+       for (int y=0; y<height; y++){
+           for (int x=0; x<width; x++){
+               maps[y][x] = type;
+           }
+       }
+   }
 
-	// fill two-dimentional arrays with a given number (maps[y][x])
-    public void fillMap(int type){
-        for (int y=0; y<height; y++){
-            for (int x=0; x<width; x++){
-                maps[y][x] = type;
-            }
-        }
-    }
+ // dig walls for creating trails
+   public void digMap(int x, int y){
+       setMap(x, y, MapData.TYPE_SPACE);
+       int[][] dl = {{0,1},{0,-1},{-1,0},{1,0}};
+       int[] tmp;
 
-	// dig walls for creating trails
-    public void digMap(int x, int y){
-        setMap(x, y, MapData.TYPE_SPACE);
-        int[][] dl = {{0,1},{0,-1},{-1,0},{1,0}};
-        int[] tmp;
-    // dlのシャッフル
-        for (int i=0; i<dl.length; i++) {   //dl.length = 4
-            int r = (int)(Math.random()*dl.length);  //range 0~3
-            tmp = dl[i];
-            dl[i] = dl[r];
-            dl[r] = tmp;
-        }
+       for (int i=0; i<dl.length; i++) {
+           int r = (int)(Math.random()*dl.length);
+           tmp = dl[i];
+           dl[i] = dl[r];
+           dl[r] = tmp;
+       }
 
-        for (int i=0; i<dl.length; i++){
-            int dx = dl[i][0];             //dx*2,dy*2 range -2~2
-            int dy = dl[i][1];
-            if (getMap(x+dx*2, y+dy*2) == MapData.TYPE_WALL){
-                setMap(x+dx, y+dy, MapData.TYPE_SPACE);
-                digMap(x+dx*2, y+dy*2);
-
-            }                             //一度止まるまで線を引く。その後先端から広がっていき根元まで処理が戻ってくる。 
-        }
-        //アイテム画像との変換
-        Random rnd = new Random();
-        int x_rnd = rnd.nextInt(22)+1;
-        int y_rnd = rnd.nextInt(16)+1;
-        for(int i=2;i<=5;i++){
-            do{
-                if(getMap(x_rnd,y_rnd) == MapData.TYPE_SPACE){
-                    mapImageViews[y_rnd][x_rnd] = new ImageView(mapImages[i]);
-                }
-            }while(getMap(x_rnd,y_rnd) == MapData.TYPE_WALL);
-        }
-        
-    }
-
+       for (int i=0; i<dl.length; i++){
+           int dx = dl[i][0];
+           int dy = dl[i][1];
+           if (getMap(x+dx*2, y+dy*2) == MapData.TYPE_WALL){
+               setMap(x+dx, y+dy, MapData.TYPE_SPACE);
+               digMap(x+dx*2, y+dy*2);
+           }
+       }
+   }
+   //アイテム画像との変換
+   public void putgate(){
+       do{
+           x_rnd = rnd.nextInt(21);
+           y_rnd = rnd.nextInt(15);
+           if(getMap(x_rnd,y_rnd) == MapData.TYPE_SPACE){
+               setMap(x_rnd,y_rnd,MapData.TYPE_GATE);
+               break;
+           }
+           }while(getMap(x_rnd,y_rnd) != MapData.TYPE_SPACE);
+   }
+   public void putswitch(){
+       do{
+           x_rnd = rnd.nextInt(21);
+           y_rnd = rnd.nextInt(15);
+           if(getMap(x_rnd,y_rnd) == MapData.TYPE_SPACE){
+               setMap(x_rnd,y_rnd,MapData.TYPE_SWITCH);
+               break;
+               }
+       }while(getMap(x_rnd,y_rnd) != MapData.TYPE_SPACE);
+   }
+   public void putturn(){
+       do{
+           x_rnd = rnd.nextInt(21);
+           y_rnd = rnd.nextInt(15);
+           if(getMap(x_rnd,y_rnd) == MapData.TYPE_SPACE){
+               setMap(x_rnd,y_rnd,MapData.TYPE_TURN);
+               break;
+           }
+       }while(getMap(x_rnd,y_rnd) != MapData.TYPE_SPACE);
+   }
+   public void putkey(){
+       do{
+           x_rnd = rnd.nextInt(21);
+           y_rnd = rnd.nextInt(15);
+           if(getMap(x_rnd,y_rnd) == MapData.TYPE_SPACE){
+               setMap(x_rnd,y_rnd,MapData.TYPE_KEY);
+               break;
+           }
+       }while(getMap(x_rnd,y_rnd) != MapData.TYPE_SPACE);
+   }
+   public boolean getIsExistKey(){
+       return isExistKey;
+   }
+   public void getKey(int x, int y, int type){
+       setMap(x, y, type);
+       isExistKey = false;
+   }
+   public void getSwitch(int x, int y, int type){
+       setMap(x, y, type);
+       isExistSwitch = false;
+   }
 }
+
 // branch test
--- contest/contest/MapGameController.java
+++ contest/contest/MapGameController.java
@@ -27,11 +27,21 @@ public class MapGameController implements Initializable {
     public Label SCORECOUNT;
     public Label STAGECOUNT;
     public Label TIMECOUNT;
-
+    public Label ITEMDATA;
+    public Label CATHP;
+    public Label DIZZY;
     //status
     private int ScoreNum=0;
     private int StageNum=0;
     private int TimeNum=60;
+    public  int KeyNum=0;
+    public  int CoinNum=0;
+    private int HpNum=100;
+    private int DizzyNum=0;
+    //
+    //ゴール判定
+    int NONE = 0;
+    int GOAL = 1;
     //
 
     //getstatus
@@ -45,6 +55,22 @@ public class MapGameController implements Initializable {
 
     public int getTimeNum(){
         return TimeNum;
+    }
+
+    public int getKeyNum(){
+        return KeyNum;
+    }
+
+    public int getCoinNum(){
+        return CoinNum;
+    }
+
+    public int getHpNum(){
+        return HpNum;
+    }
+
+    public int getDizzyNum(){
+        return DizzyNum;
     }
     //
 
@@ -67,8 +93,31 @@ public class MapGameController implements Initializable {
       SCORECOUNT.setText("" + getScoreNum());
       STAGECOUNT.setText("" + getStageNum());
       TIMECOUNT.setText("" + getTimeNum() + "s");
+      ITEMDATA.setText("key" + getKeyNum() + " " + "coin" + getCoinNum());
+      CATHP.setText("" + getHpNum());
+      DIZZY.setText("" + getDizzyNum());
   }
   //
+
+   //制限時間の実装//
+   //public class Countdown {
+
+        //final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        //final Runnable runnable = new Runnable() {
+          //  public void run() {
+                //TimeNum--;
+
+                ///if (TimrNum < 0) {
+                    //System.out.println("Timer Over!");
+                    //scheduler.shutdown();
+                //}
+          //  }
+        //};
+        //scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
+
+//}
+   //
 
     // Draw the map
     public void drawMap(MoveChara c, MapData m){
@@ -89,64 +138,74 @@ public class MapGameController implements Initializable {
     }
     //
 
-
-
-
     // Get users key actions
-    public void keyAction(KeyEvent event){
-        KeyCode key = event.getCode(); System.out.println("keycode:"+key);
-        if (key == KeyCode.A){
-        	leftButtonAction();
-        }else if (key == KeyCode.S){
-            downButtonAction();
-        }else if (key == KeyCode.W){
-            upButtonAction();
-        }else if (key == KeyCode.D){
-            rightButtonAction();
-        }
-    }
+   public void keyAction(KeyEvent event){
+       KeyCode key = event.getCode(); System.out.println("keycode:"+key);
+       if (key == KeyCode.A){
+         leftButtonAction();
+       }else if (key == KeyCode.S){
+           downButtonAction();
+       }else if (key == KeyCode.W){
+           upButtonAction();
+       }else if (key == KeyCode.D){
+           rightButtonAction();
+       }
+   }
 
-    // Operations for going the cat down
-    public void upButtonAction(){
-        printAction("UP");
-        chara.setCharaDirection(MoveChara.TYPE_UP);
-        chara.move(0, -1);
-        drawMap(chara, mapData);
-    }
+   // Operations for going the cat down
+   public void upButtonAction(){
+       printAction("UP");
+       chara.setCharaDirection(MoveChara.TYPE_UP);
+       chara.move(0, -1);
+       drawMap(chara, mapData);
+   }
 
-    // Operations for going the cat down
-    public void downButtonAction(){
-        printAction("DOWN");
-        chara.setCharaDirection(MoveChara.TYPE_DOWN);
-        chara.move(0, 1);
-        drawMap(chara, mapData);
-    }
+   // Operations for going the cat down
+   public void downButtonAction(){
+       printAction("DOWN");
+       chara.setCharaDirection(MoveChara.TYPE_DOWN);
+       chara.move(0, 1);
+       drawMap(chara, mapData);
+   }
 
-    // Operations for going the cat right
-    public void leftButtonAction(){
-        printAction("LEFT");
-        chara.setCharaDirection(MoveChara.TYPE_LEFT);
-        chara.move(-1, 0);
-        drawMap(chara, mapData);
-    }
+   // Operations for going the cat right
+   public void leftButtonAction(){
+       printAction("LEFT");
+       chara.setCharaDirection(MoveChara.TYPE_LEFT);
+       chara.move(-1, 0);
+       drawMap(chara, mapData);
+   }
 
-    // Operations for going the cat right
-    public void rightButtonAction(){
-        printAction("RIGHT");
-        chara.setCharaDirection(MoveChara.TYPE_RIGHT);
-        chara.move(1, 0);
-        drawMap(chara, mapData);
-    }
+   // Operations for going the cat right
+   public void rightButtonAction(){
+       printAction("RIGHT");
+       chara.setCharaDirection(MoveChara.TYPE_RIGHT);
+       chara.move(1, 0);
+       drawMap(chara, mapData);
+   }
 
-    public void func1ButtonAction(ActionEvent event) {
-        System.out.println("func1: Nothing to do");
-    }
+   public void func1ButtonAction(ActionEvent event) {
+       System.out.println("func1: Nothing to do");
+   }
 
-    // Print actions of user inputs
-    public void printAction(String actionString) {
-        System.out.println("Action: " + actionString);
-    }
-
-
-
+   // Print actions of user inputs
+   public void printAction(String actionString) {
+       System.out.println("Action: " + actionString);
+   }
+   //アイテム取得とゴール確認
+   public int moveCheck(){
+       int cx = chara.getPosX();
+       int cy = chara.getPosY();
+       int cm = mapData.getMap(cx, cy);
+       if (cm == MapData.TYPE_KEY) {
+           System.out.println("GET KEY");
+           mapData.getKey(cx, cy, MapData.TYPE_SPACE);
+       } else if (cm == MapData.TYPE_SWITCH) {
+           System.out.println("GET SWITCH");
+           mapData.getKey(cx, cy, MapData.TYPE_SPACE);
+       } else if (cm == MapData.TYPE_GATE && ! mapData.getIsExistKey()){
+           return GOAL;
+       }
+       return NONE;
+   }
 }
